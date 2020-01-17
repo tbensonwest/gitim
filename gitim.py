4f2f0501dcf42820a3051795b298299a16eb72ce
tbensonwest <tarran@benson-west.com>
2020-01-17 11:39:12 +0000

    Uses config file instead of args, and able to use enterprise github.

--- gitim.py
+++ gitim.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 
 from __future__ import print_function
+import json
 from getpass import getpass
 from argparse import ArgumentParser
 from os import chdir, path, makedirs, pardir, environ
@@ -37,6 +38,16 @@ created by {__author__}
 Version: {__version__}
 """.format(__author__=__author__, __version__=__version__))
 
+        with open('dev_config.json') as config_file:
+            config = json.load(config_file)
+
+        self.token = config['github_token']
+        self.hostname = config['hostname']
+        self.org = config['org']
+        self.dest = config['dest']
+        self.ssh = config['ssh']
+        self.nopull = config['nopull']
+
     def set_args(self):
         """ Create parser for command line arguments """
         parser = ArgumentParser(
@@ -50,12 +61,16 @@ Version: {__version__}
         parser.add_argument('--nopull', action='store_true', help=u'Don\'t pull if repository exists. [false]')
         parser.add_argument('--shallow', action='store_true', help=u'Perform shallow clone. [false]')
         parser.add_argument('--ssh', action='store_true', help=u'Use ssh+git urls for checkout. [false]')
+        parser.add_argument('-e', '--ent', help=u'Enterprise Account domain. github.com used by default.')
         return parser
 
     def make_github_agent(self, args):
         """ Create github agent to auth """
         if args.token:
             g = Github(args.token)
+        elif self.token and self.hostname:
+            base_url = f"https://{self.hostname}/api/v3"
+            g = Github(base_url=base_url, login_or_token=self.token)
         else:
             user = args.user
             password = args.password
@@ -73,33 +88,34 @@ Version: {__version__}
         parser = self.set_args()
         args = parser.parse_args()
         g = self.make_github_agent(args)
-        user = g.get_user().login
+        # g.get_user().login
         # (BadCredentialsException, TwoFactorException, RateLimitExceededException)
 
         join = path.join
-        if args.dest:
-            if not path.exists(args.dest):
-                makedirs(args.dest)
-                print(u'mkdir -p "{}"'.format(args.dest))
-            join = partial(path.join, args.dest)
+        if self.dest:
+            if not path.exists(self.dest):
+                makedirs(self.dest)
+                print(u'mkdir -p "{}"'.format(self.dest))
+            join = partial(path.join, self.dest)
 
-        get_repos = g.get_organization(args.org).get_repos if args.org else g.get_user().get_repos
+        get_repos = g.get_organization(self.org).get_repos() if args.org else g.get_user().get_repos
         for repo in get_repos():
-            if not path.exists(join(repo.name)):
-                clone_url = repo.clone_url
-                if args.ssh:
-                    clone_url = repo.ssh_url
-                if args.shallow:
-                    print(u'Shallow cloning "{repo.full_name}"'.format(repo=repo))
-                    call([u'git', u'clone', '--depth=1', clone_url, join(repo.full_name)])
+            if self.org in repo.full_name:
+                if not path.exists(join(repo.name)):
+                    clone_url = repo.clone_url
+                    if self.ssh:
+                        clone_url = repo.ssh_url
+                    if args.shallow:
+                        print(u'Shallow cloning "{repo.full_name}"'.format(repo=repo))
+                        # call([u'git', u'clone', '--depth=1', clone_url, join(repo.full_name)])
+                    else:
+                        print(u'Cloning "{repo.full_name}"'.format(repo=repo))
+                        # call([u'git', u'clone', clone_url, join(repo.full_name)])
+                elif not self.nopull:
+                    print(u'Updating "{repo.name}"'.format(repo=repo))
+                    # call([u'git', u'pull'], env=dict(environ, GIT_DIR=join(repo.name, '.git').encode('utf8')))
                 else:
-                    print(u'Cloning "{repo.full_name}"'.format(repo=repo))
-                    call([u'git', u'clone', clone_url, join(repo.full_name)])
-            elif not args.nopull:
-                print(u'Updating "{repo.name}"'.format(repo=repo))
-                call([u'git', u'pull'], env=dict(environ, GIT_DIR=join(repo.name, '.git').encode('utf8')))
-            else:
-                print(u'Already cloned, skipping...\t"{repo.full_name}"'.format(repo=repo))
+                    print(u'Already cloned, skipping...\t"{repo.full_name}"'.format(repo=repo))
         print(u'FIN')
 
 
